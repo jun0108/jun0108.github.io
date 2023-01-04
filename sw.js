76582e2a02a72837c264ad94606643e5e4936ded
jason0814 <jason0814@iabacus.co.kr>
2023-01-05 08:37:25 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3cf5a7cbb23f324eea33.js"
+    "url": "webpack-runtime-b2cab343520e757cbecc.js"
   },
   {
     "url": "styles.853e65090dc89bfbb4ac.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "04cf455bab21de7ec835c6fba9083a38"
+    "revision": "c65a4f04d4111f542a8e8021ea1d8b6e"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
